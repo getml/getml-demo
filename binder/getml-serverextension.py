1c64f3994f43fae7b2ba4bc10ee2ab9fc93fc72c
Sören Nikolaus <soeren.nikolaus@getml.com>
2020-08-25 13:34:25 +0200

    Add reading of environment

--- binder/getml-serverextension.py
+++ binder/getml-serverextension.py
@@ -2,22 +2,41 @@ import os
 import sys
 import re
 import requests as req
+import pathlib
 from subprocess import Popen, PIPE, STDOUT
+# from getml_binder import binder_init
+
+
+def get_environment(getml_dir):
+    getml_env_file = getml_dir / "environment.json"
+    binder_ref = os.environ.get("BINDER_REF_URL")
+    client_id = os.environ.get("JUPYTERHUB_CLIENT_ID")
+
+    with open(getml_env_file) as f:
+        getml_env = json.load(f)
+
+    license_seed = getml_env['monitor']['licenseSeedStatic']
+
+    return binder_ref, client_id, license_seed
 
 
 def load_jupyter_server_extension(nbapp):
     web_app = nbapp.web_app
     user_base = web_app.settings['base_url']
+    home = pathlib.Path.home()
+    getml_dir = home / "binder/getml"
 
-    with open("/home/jovyan/binder/getml.log", "wb") as out, open("/home/jovyan/binder/stderr.log", "wb") as err:
-        Popen(["./getML", "--install", "--proxy-url",
-               user_base+"proxy/1709", "--http-port", "1709"],
-              cwd="/home/jovyan/binder/getml/", stdout=out, stderr=err)
+    with open(home / "binder/getml.log", "wb") as out, open(home / "binder/stderr.log", "wb") as err:
+        install = Popen(["./getML", "--install", "--proxy-url",
+                         user_base+"proxy/1709", "--http-port", "1709"],
+                        cwd=getml_dir, stdout=out, stderr=err)
+        install.wait()
+        env = get_environment(getml_dir)
 
     # pass base url to markdown
-    with open('/home/jovyan/welcome.md', 'r+') as f:
+    with open(home/'welcome.md', 'r+') as f:
         content = f.read()
         f.seek(0)
         f.write(re.sub(r'(http:\/\/localhost:1709)',
-                       user_base + r'proxy/1709/', content))
+                       user_base + r'proxy/1709/', content, '\n\n', env))
         f.truncate()
