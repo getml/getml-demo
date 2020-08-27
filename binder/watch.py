c5a847e81d65eb0dc92070375fccd1ec02aa27de
Alexander uhlig <alex@getml.com>
2020-08-27 23:49:39 +0200

    Small fix

--- binder/watch.py
+++ binder/watch.py
@@ -1,7 +1,9 @@
 from getml_binder_init import watch_log, get_environment
 from pathlib import Path
+import sys
 
 home = Path.home()
-env = get_environment(home / ".getML")
-print(env)
-watch_log(home / "binder/getml.out.log", env)
+sys.stdout.write(home)
+env = get_environment("/home/jovyan/.getML")
+sys.stdout.write(env)
+watch_log("/home/jovyan/binder/getml.out.log", env)
