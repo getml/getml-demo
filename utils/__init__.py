5111c6ce9a34dc65d438616689b827efb3e18e37
Patrick Urbanke <patrick@getml.com>
2021-12-29 21:41:34 +0100

    Added the CesiumMLBuilder

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from .benchmark import Benchmark
+from .cesiumml_builder import CesiumMLBuilder
 from .ft_time_series_builder import FTTimeSeriesBuilder
 from .load import load_or_query, load_or_retrieve
 from .ts_fresh_builder import TSFreshBuilder
