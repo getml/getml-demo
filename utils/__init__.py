971b44cda7ed9dd2e5aa13a9d19b2c701862bdfa
Sören Nikolaus <soeren.nikolaus@getml.com>
2022-01-19 10:53:37 +0100

    Add first version of TSFELBuilder

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from .cesiumml_builder import CesiumMLBuilder
 from .ft_time_series_builder import FTTimeSeriesBuilder
 from .load import load_or_query, load_or_retrieve
 from .ts_fresh_builder import TSFreshBuilder
+from .tsfel_builder import TSFELBuilder
