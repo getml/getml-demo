5748b9369e5547c19db5a3c572ce162c433b5b41
Sören Nikolaus <soeren.nikolaus@getml.com>
2022-01-24 11:32:14 +0100

    Bring KATSBuilder to utils namespace

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
 from .benchmark import Benchmark
 from .cesiumml_builder import CesiumMLBuilder
 from .ft_time_series_builder import FTTimeSeriesBuilder
+from .kats_builder import KATSBuilder
 from .load import load_or_query, load_or_retrieve
 from .ts_fresh_builder import TSFreshBuilder
 from .tsfel_builder import TSFELBuilder
