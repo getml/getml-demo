4de45ca0dba7a383d108831ddf9435f816d87884
Sören Nikolaus <soeren.nikolaus@getml.com>
2022-02-11 17:26:52 +0100

    Finalize and refactor TSFELBuilder

--- utils/tsfel_builder.py
+++ utils/tsfel_builder.py
@@ -5,17 +5,18 @@ Wrapper around TSFEL
 import datetime
 import time
 import warnings
-from typing import Dict, List, Optional, Tuple, TypedDict, Union
+from typing import Dict, Iterator, List, Optional, TypedDict, Union
 
 import numpy as np
 import pandas as pd  # type: ignore
-import tsfel
 from pandas.api.types import is_numeric_dtype  # type: ignore
-from scipy.stats import pearsonr  # type: ignore
+from tsfel.feature_extraction.calc_features import calc_window_features
+from tsfel.feature_extraction.features_settings import (
+    get_features_by_domain,  # type: ignore
+)
 
-from .add_original_columns import _add_original_columns
 from .print_time_taken import _print_time_taken
-from .remove_target_column import _remove_target_column
+from .rolling import _roll_data_frame
 
 
 class TSFELAgg(TypedDict):
@@ -35,6 +36,52 @@ class TSFELAgg(TypedDict):
 TSFELAggs = Dict[str, Dict[str, TSFELAgg]]
 
 # ------------------------------------------------------------------
+
+
+def _aggregate_chunk(
+    chunk: pd.DataFrame,
+    column_id: str,
+    time_stamp: str,
+    aggregations: TSFELAggs,
+) -> pd.DataFrame:
+    orig_colnames = [col for col in chunk.columns if col not in (time_stamp, column_id)]
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        data = calc_window_features(
+            signal_window=chunk[orig_colnames],
+            dict_features=aggregations,
+            window_size=len(chunk),
+            verbose=0,
+            fs=100,
+            n_jobs=None,
+        )
+
+    colnames = [
+        f"{agg[2:]} ({cname})" for cname in orig_colnames for agg in data.columns
+    ]
+
+    data.columns = colnames
+
+    return data
+
+
+# -------------------------------------------------------------------
+
+
+def _aggregate_chunks(
+    rolled: Iterator[pd.DataFrame],
+    column_id: str,
+    time_stamp: str,
+    aggregations: TSFELAggs,
+) -> pd.DataFrame:
+    aggregated_chunks: List[pd.DataFrame] = [
+        _aggregate_chunk(chunk, column_id, time_stamp, aggregations) for chunk in rolled
+    ]
+    return pd.concat(aggregated_chunks, ignore_index=True).reset_index()
+
+
+# -------------------------------------------------------------------
 
 
 def _flatten_aggs(
@@ -70,28 +117,11 @@ def _infer_required_obs(aggs: TSFELAggs) -> int:
 # ------------------------------------------------------------------
 
 
-def _get_window_params(
-    data_frame: pd.DataFrame,
-    horizon: pd.Timedelta,
-    memory: pd.Timedelta,
-) -> Tuple[float, float, float]:
-    freq = pd.Timedelta(1, pd.infer_freq(data_frame.index))
-    win_size = memory // freq
-    lag = horizon // freq
-    overlap = (win_size - lag) / win_size
-    return lag, win_size, overlap
-
-
-# ------------------------------------------------------------------
-
-
 class TSFELBuilder:
     """
     Scikit-learn-style feature builder based on TSFEL.
 
     Args:
-        aggregations: The aggregations to use.
-
         num_features: The (maximum) number of features to build.
 
         horizon: The prediction horizon to use.
@@ -99,13 +129,22 @@ class TSFELBuilder:
         memory: How much back in time you want to go until the
                 feature builder starts "forgetting" data.
 
+        column_id: The name of the column containing the ids.
+
         time_stamp: The name of the column containing the time stamps.
 
         target: The name of the target column.
+
+        aggregations: The aggregations to use.
+
+        allow_lagged_targets: Whether to build features based on lagged targets
+
+        min_chunksize: The minimum size of chunks to aggregate over
+
     """
 
-    statistical_aggs: TSFELAggs = tsfel.get_features_by_domain("statistical")
-    temporal_aggs: TSFELAggs = tsfel.get_features_by_domain("temporal")
+    statistical_aggs: TSFELAggs = get_features_by_domain("statistical")
+    temporal_aggs: TSFELAggs = get_features_by_domain("temporal")
     # spectral_aggs: TSFELAggs = tsfel.get_features_by_domain("spectral")
     # all_aggs: TSFELAggs = tsfel.get_features_by_domain()
     all_aggs: TSFELAggs = {**statistical_aggs, **temporal_aggs}
@@ -115,66 +154,52 @@ class TSFELBuilder:
         num_features: int,
         horizon: pd.Timedelta,
         memory: pd.Timedelta,
+        column_id: str,
         time_stamp: str,
         target: str,
         aggregations: Optional[TSFELAggs] = None,
         allow_lagged_targets: bool = False,
+        min_chunksize: Optional[int] = None,
     ) -> None:
-        self.aggregations = aggregations or self.all_aggs
         self.num_features = num_features
         self.horizon = horizon
         self.memory = memory
+        self.column_id = column_id
         self.time_stamp = time_stamp
         self.target = target
+        self.aggregations = aggregations or self.all_aggs
         self.allow_lagged_targets = allow_lagged_targets
+        self.min_chunksize = min_chunksize or _infer_required_obs(self.aggregations)
 
         self._runtime = None
         self.fitted = False
-        self.max_depth = 2
 
         self.selected_features: List[int] = []
 
     def _extract_features(self, data_frame: pd.DataFrame) -> pd.DataFrame:
-        data_frame = data_frame.set_index(self.time_stamp)
-        lag, window_size, overlap = _get_window_params(
-            data_frame, self.horizon, self.memory
+        data_frame = data_frame.reset_index(drop=True)
+        rolled = _roll_data_frame(
+            data_frame, self.time_stamp, self.horizon, self.memory, self.min_chunksize
         )
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            df_extracted = tsfel.time_series_features_extractor(
-                self.aggregations,
-                data_frame,
-                window_size=window_size,
-                overlap=overlap,
-                verbose=0,
-            )
-        df_extracted.set_index(data_frame.index[window_size - lag :])
-        for col in df_extracted:
-            if is_numeric_dtype(df_extracted[col]):
-                df_extracted[col][df_extracted[col].isna()] = 0
-        return df_extracted
+        extracted = _aggregate_chunks(
+            rolled, self.column_id, self.time_stamp, self.aggregations
+        )
+        for col in extracted:
+            if is_numeric_dtype(extracted[col]):
+                extracted[col][extracted[col].isna()] = 0
+        return extracted
 
     def _select_features(
         self, data_frame: pd.DataFrame, target: Union[pd.Series, np.ndarray]
     ) -> pd.DataFrame:
-        colnames = np.asarray(data_frame.columns)
-        print("Selecting the best out of " + str(len(colnames)) + " features...")
-        colnames = np.asarray(
-            [
-                col
-                for col in colnames
-                if is_numeric_dtype(data_frame[col])
-                and np.var(np.asarray(data_frame[col])) > 0.0
-            ]
-        )
-        correlations = np.asarray(
-            [np.abs(pearsonr(target, data_frame[col]))[0] for col in colnames]
-        )
-        correlations[np.isnan(correlations) | np.isinf(correlations)] = 0.0
+        print(f"Selecting the best out of {data_frame.shape[1]} features...")
 
-        self.selected_features = colnames[np.argsort(correlations)][::-1][
-            : self.num_features
-        ]
+        data_frame = data_frame.loc[:, (data_frame != data_frame.iloc[0]).any()]
+        correlations = data_frame.corrwith(pd.Series(target)).abs()
+        correlations = correlations.replace([np.inf, np.nan], 0)
+        correlations = correlations.sort_values(ascending=False)
+
+        self.selected_features = correlations.index[: self.num_features].tolist()
         return data_frame[self.selected_features]
 
     def fit(self, data_frame: pd.DataFrame) -> pd.DataFrame:
@@ -184,38 +209,24 @@ class TSFELBuilder:
         """
         print("TSFEL: Trying features...")
         begin = time.time()
-        target = np.asarray(data_frame[self.target])
-        df_for_extraction = (
-            data_frame
-            if self.allow_lagged_targets
-            else _remove_target_column(data_frame, self.target)
-        )
-        df_extracted = self._extract_features(df_for_extraction)
-        cutoff = len(target) - len(df_extracted)
-        df_selected = self._select_features(df_extracted, target[cutoff:])
-        df_selected = _add_original_columns(data_frame[cutoff:], df_selected)
+        grouped = data_frame.groupby(self.column_id)
+        extracted = []
+        other = []
+        for _, group in grouped:
+            to_extract = group if self.allow_lagged_targets else group.drop(self.target)
+            extracted.append(self._extract_features(to_extract))
+            other.append(group.drop(to_extract.colnames)[self.min_chunksize :])
+        extracted = pd.concat(extracted)
+        other = pd.concat(other).reset_index(drop=True)
+        selected = self._select_features(extracted, other[self.target])
+        selected = pd.concat([selected, other], axis=1)
         end = time.time()
         _print_time_taken(begin, end)
         self.fitted = True
         self._runtime = datetime.timedelta(seconds=end - begin)
-        return df_selected
+        return selected
 
     @property
     def runtime(self) -> Union[None, datetime.timedelta]:
         if self.fitted:
             return self._runtime
-
-    def transform(self, data_frame: pd.DataFrame) -> pd.DataFrame:
-        """
-        Fits the DFS on the data frame and returns
-        the features for the training set.
-        """
-        df_for_extraction = (
-            data_frame
-            if self.allow_lagged_targets
-            else _remove_target_column(data_frame, self.target)
-        )
-        df_extracted = self._extract_features(df_for_extraction)
-        df_selected = df_extracted[self.selected_features]
-        df_selected = _add_original_columns(data_frame, df_selected)
-        return df_selected
