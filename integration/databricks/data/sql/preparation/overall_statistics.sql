e70b99a5930decd9b0c2683e3628eff178515702
Awais Mirza <47096947+awaismirza92@users.noreply.github.com>
2025-12-18 10:15:36 +0100

    Remove SQL loading module and display functions & SQL files

--- integration/databricks/data/sql/preparation/overall_statistics.sql
+++
@@ -1,8 +0,0 @@
--- Overall statistics
-SELECT 
-    COUNT(*) as total_snapshots,
-    COUNT(DISTINCT store_id) as num_stores,
-    AVG(next_week_sales) as avg_weekly_sales,
-    SUM(next_week_sales) as total_sales,
-    SUM(next_week_orders) as total_orders
-FROM IDENTIFIER(:population_table);
